a4cfa207d2276db00ef0dd195b8cc95b292f20de
Elita <elita.fernandes@newicon.net>
2017-05-07 12:44:51 +0100

    fixed issues

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -27,11 +27,14 @@
         	{!! $article[0]->content !!}
         	
         </div>
+        <!-- big hori -->
         <ins class="adsbygoogle"
              style="display:inline-block;width:728px;height:90px"
              data-ad-client="ca-pub-1723053501613692"
              data-ad-slot="6490772110"></ins>
         <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         <div id="disqus_thread"></div>
     </div>
 
